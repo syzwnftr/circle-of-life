86b36c789fca1ac7232c0e279d793c534c6b11ea
Syazwan Fitri <syazfitry69@gmail.com>
2022-08-27 14:35:42 +0800

    Added Footer compenent to the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import About from './components/about/About';
+import Benefit from './components/benefit/Benefit';
+import Fit from './components/fit/Fit';
+import Footer from './components/footer/Footer';
 import Hero from './components/hero/Hero';
+import Process from './components/process/Process';
+import Stats from './components/stats/Stats';
+import Thing from './components/things/Thing';
 
 function App() {
   return(
     <div className='app'>
       <Hero />
       <About />
+      <Stats />
+      <Process />
+      <Benefit />
+      <Thing />
+      <Fit />
+      <Footer />
     </div>
   )
 }
