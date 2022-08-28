f8417d4c8157c4a0b11fa39c45b44b34d3895c4d
Syazwan Fitri <syazfitry69@gmail.com>
2022-08-28 16:02:18 +0800

    Added Stats component

---
+++ src/components/stats/Stats.jsx
@@ -0,0 +1,25 @@
+import './Stats.css';
+
+function Stats() {
+    return (
+        <div className='s'>
+            <div className="s-item">
+                <h3 className="s-num">92 000 000 <span>t/year</span></h3>
+
+                <p className="s-desc">
+                    the volume of waste from fashion industry, which is the second place in terms of damage tonature
+                </p>
+            </div>
+
+            <div className="s-item">
+                <h3 className="s-num">1 029 000 <span>t/year</span></h3>
+
+                <p className="s-desc">
+                    produce by Indonesia. Bali has already declared on emergency over waste in the ocean
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Stats;
