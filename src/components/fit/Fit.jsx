d63bd7a8b8fc9aa71ffd37341217541d2bc54a91
Syazwan Fitri <syazfitry69@gmail.com>
2022-08-30 22:06:31 +0800

    Added wrapper, img asset, class

--- src/components/fit/Fit.jsx
+++ src/components/fit/Fit.jsx
@@ -3,6 +3,7 @@ import Applebadge from '../../img/applebadge.png';
 import Googlebadge from '../../img/googlebadge.png';
 import Mapicon from '../../img/mapicon.png';
 import Greenapp from '../../img/greenapp.png';
+import Kotak from '../../img/kotak.png';
 
 
 function Fit() {
@@ -36,53 +37,57 @@ function Fit() {
                     <button className="f-btn no">No</button>
                 </div>
 
-                <div className="f-item">
-                    <img src={Greenapp} alt="" className="f-img" />
+                <div className="f-item-box">
+                    <div className="f-item-wrapper">
+                        <div className="f-item">
+                            <img src={Greenapp} alt="" className="f-img" />
 
-                    <div className="f-info">
-                        <div className="f-info-wrapper">
-                            <div className="f-circle">
-                                <p className="f-num">1</p>
+                            <div className="f-info">
+                                <div className="f-info-wrapper">
+                                    <div className="f-circle">
+                                        <p className="f-num">1</p>
+                                    </div>
+                                    <h3 className="f-subhead">
+                                        Download the taxi application
+                                    </h3>
+                                </div>
+
+                                <div className="f-badge-wrapper">
+                                    <a href="https://play.google.com" target="_blank" rel="noreferrer">
+                                        <img src={Applebadge} alt="" className="f-badge apple" />
+                                    </a>
+
+                                    <a href="https://play.google.com/" target="_blank" rel="noreferrer">
+                                        <img src={Googlebadge} alt="" className="f-badge" />
+                                    </a>
+                                </div>
                             </div>
-                            <h3 className="f-subhead">
-                                Download the taxi application
-                            </h3>
                         </div>
 
-                        <div className="f-badge-wrapper">
-                            <a href="https://play.google.com" target="_blank" rel="noreferrer">
-                                <img src={Applebadge} alt="" className="f-badge" />
-                            </a>
+                        <div className="f-item">
+                            <img src={Mapicon} alt="" className="f-img mapicon" />
 
-                            <a href="https://play.google.com/" target="_blank" rel="noreferrer">
-                                <img src={Googlebadge} alt="" className="f-badge" />
-                            </a>
+                            <div className="f-info">
+                                <div className="f-info-wrapper">
+                                    <div className="f-circle">
+                                        <p className="f-num">2</p>
+                                    </div>
+                                    <h3 className="f-subhead">
+                                        Indicate the warehouse address
+                                    </h3>
+                                </div>
+
+                                <div className="f-badge-wrapper">
+                                    <p className="f-desc">
+                                        No 23, Jalan Gudang Sejahtera, Taman Gudang Sejahtera, 21000 Senai, Johor
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-
-                <div className="f-item">
-                    <img src={Mapicon} alt="" className="f-img mapicon" />
-
-                    <div className="f-info">
-                        <div className="f-info-wrapper">
-                            <div className="f-circle">
-                                <p className="f-num">2</p>
-                            </div>
-                            <h3 className="f-subhead">
-                                Indicate the warehouse address
-                            </h3>
-                        </div>
-
-                        <div className="f-badge-wrapper">
-                            <p className="f-desc">
-                                No 23, Jalan Gudang Sejahtera, Taman Gudang Sejahtera, 21000 Senai, Johor
-                            </p>
-                        </div>
-
-                    </div>
-                </div>
+                    <img src={Kotak} alt="" className="f-box" />
+                </div>  
             </div>
         </div>
     )
